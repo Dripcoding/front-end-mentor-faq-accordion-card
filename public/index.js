d2be55c999bf2d6fe98252898ca073203f367f1c
Dripcoding <dripcoding@gmail.com>
2020-10-11 18:35:41 -0700

    set up index.js

---
+++ public/index.js
@@ -0,0 +1,15 @@
+// styles
+import './styles/reset.scss'
+import './styles/main.scss'
+// fonts
+import './assets/fonts/KumbhSans-Bold.ttf'
+import './assets/fonts/KumbhSans-Light.ttf'
+import './assets/fonts/KumbhSans-Regular.ttf'
+// images
+import bgPatternDesktop from './assets/images/bg-pattern-desktop.svg'
+import bgPatternMobile from './assets/images/bg-pattern-mobile.svg'
+import favicon from './assets/images/favicon-32x32.png'
+import chevron from './assets/images/icon-arrow-down.svg'
+import boxDesktop from  './assets/images/illustration-box-desktop.svg'
+import womanOnlineDesktop from './assets/images/illustration-woman-online-desktop.svg'
+import womanOnlineMobile from './assets/images/illustration-woman-online-mobile.svg'

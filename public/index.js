d5eae5235c03de692e88cd88a2c45066f5091ba7
Dripcoding <dripcoding@gmail.com>
2020-10-16 12:45:01 -0700

    implement faqContainer__item__header--active styles

--- public/index.js
+++ public/index.js
@@ -13,12 +13,15 @@ import './styles/main.scss'
 const faqItems = document.querySelectorAll('.faqContainer__item')
 const chevrons = document.querySelectorAll('.faqContainer__item__chevron');
 const faqItemHeaders = document.querySelectorAll('.faqContainer__item__header');
+const faqItemAnswers = document.querySelectorAll('.faqContainer__item__answer')
 
 // ===== TOGGLE ACTIVE MODIFIERS =====
 for (let i = 0; i < faqItems.length; i++) {
     // listen for clicks on items
     faqItems[i].addEventListener('click', () => {
         chevrons[i].classList.toggle("faqContainer__item__chevron--active")
+        faqItems[i].classList.toggle("faqContainer__item--active")
         faqItemHeaders[i].classList.toggle("faqContainer__item__header--active")
+        faqItemAnswers[i].classList.toggle("faqContainer__item__answer--active")
     })
 }
